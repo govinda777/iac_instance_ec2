d2715a5e4ffdf72c66a20ff2b341a9fb70c69e02
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-02-06 12:27:10 -0300

    up

--- jenkins/step_get_plan_to_the_tf_controller.sh
+++ jenkins/step_get_plan_to_the_tf_controller.sh
@@ -10,7 +10,7 @@ PLAN_ID=$(kubectl get terraform iac-instance-ec2 -n flux-system -o jsonpath='{.s
 echo "ID do Plano: $PLAN_ID"
 
 # Caminho para o arquivo iac-instance-ec2_terraform.yaml no repositório local
-YAML_FILE="/path/to/demo-tf-controller/flux_tf_controller/clusters/my-cluster/gitrepositories/iac-instance-ec2_terraform.yaml"
+YAML_FILE="demo-tf-controller/flux_tf_controller/clusters/my-cluster/gitrepositories/iac-instance-ec2_terraform.yaml"
 
 # Atualizar o arquivo YAML com o ID do plano
 sed -i "s/^\(\s*approvePlan:\s*\).*$/\1\"$PLAN_ID\"/" $YAML_FILE
@@ -18,7 +18,7 @@ sed -i "s/^\(\s*approvePlan:\s*\).*$/\1\"$PLAN_ID\"/" $YAML_FILE
 echo "Arquivo YAML atualizado com o ID do plano."
 
 # Navegar para o diretório do repositório
-cd /path/to/demo-tf-controller
+cd demo-tf-controller
 
 # Adicionar, commitar e enviar as alterações
 git add $YAML_FILE
