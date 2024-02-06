215537b6bee96496018a617cdb6610590b673dd6
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-02-06 19:22:28 -0300

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+sudo chown 1000:1000 /Users/gosouza/.minikube/ca.crt
+sudo chown 1000:1000 /Users/gosouza/.minikube/profiles/flux/client.crt
+sudo chown 1000:1000 /Users/gosouza/.minikube/profiles/flux/client.key
+
+chmod 644 /Users/gosouza/.minikube/ca.crt
+chmod 644 /Users/gosouza/.minikube/profiles/flux/client.crt
+chmod 600 /Users/gosouza/.minikube/profiles/flux/client.key
+
+docker-compose down
 
 # Nome do contêiner e da imagem
 CONTAINER_NAME="jenkins-local"
@@ -18,14 +27,25 @@ if [ -n "$container_id" ]; then
     docker rm -f $container_id
 fi
 
+echo "Alterando as permissões dos arquivos de configuração do Kubernetes..."
+
+sudo chown 1000:1000 /Users/gosouza/.minikube/ca.crt
+sudo chown 1000:1000 /Users/gosouza/.minikube/profiles/flux/client.crt
+sudo chown 1000:1000 /Users/gosouza/.minikube/profiles/flux/client.key
+
 # Executar um novo contêiner Jenkins
 echo "Iniciando um novo contêiner Jenkins..."
-docker run -d -p 8080:8080 -p 50000:50000 \
-  -v jenkins_home:/var/jenkins_home \
-  -v $(pwd)/kubeconfig:/var/jenkins_home/.kube/config \
-  -v /Users/gosouza/.minikube/ca.crt:/var/jenkins_home/.kube/ca.crt \
-  -v /Users/gosouza/.minikube/profiles/flux/client.crt:/var/jenkins_home/.kube/client.crt \
-  -v /Users/gosouza/.minikube/profiles/flux/client.key:/var/jenkins_home/.kube/client.key \
-  --name ${CONTAINER_NAME} ${IMAGE_NAME}
+
+docker-compose up -d
+
+# docker run -d -p 8080:8080 -p 50000:50000 \
+#   -v jenkins_home:/var/jenkins_home \
+#   -v $(pwd)/kubeconfig:/var/jenkins_home/.kube/config \
+#   -v /Users/gosouza/.minikube/ca.crt:/var/jenkins_home/.kube/ca.crt \
+#   -v /Users/gosouza/.minikube/profiles/flux/client.crt:/var/jenkins_home/.kube/client.crt \
+#   -v /Users/gosouza/.minikube/profiles/flux/client.key:/var/jenkins_home/.kube/client.key \
+#   --name ${CONTAINER_NAME} ${IMAGE_NAME}
+
+
 
 docker exec -it jenkins-local bash
